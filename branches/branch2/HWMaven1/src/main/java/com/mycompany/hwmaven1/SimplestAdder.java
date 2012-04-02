dcc941f8fb0474f1e385a31891234ea233922d72
eanhernandez@eanh.net <eanhernandez@eanh.net@225b7d01-1364-4f71-3ed7-4e46c0c686c8>
2012-04-02 05:01:56 +0000

    asdf

---
+++ branches/branch2/HWMaven1/src/main/java/com/mycompany/hwmaven1/SimplestAdder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hwmaven1;
+
+/**
+ *
+ * @author ean
+ */
+public class SimplestAdder {
+    
+    public int addit(int x, int y)
+    {
+        
+        return x + y;
+    }
+    
+}
