a7a9b047f47c532eb0baf9972d293fff3cc4e7d0
eanhernandez@eanh.net <eanhernandez@eanh.net@225b7d01-1364-4f71-3ed7-4e46c0c686c8>
2012-04-07 18:46:39 +0000

    again

---
+++ trunk/HWMaven1/src/test/java/com/mycompany/hwmaven1/NewTestSuite.java
@@ -0,0 +1,29 @@
+package com.mycompany.hwmaven1;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author ean
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(value={SimplestAdderTest.class})
+public class NewTestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
