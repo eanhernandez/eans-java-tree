3ece6829f13ae94288606b09f81e236898834f28
eanhernandez@eanh.net <eanhernandez@eanh.net@225b7d01-1364-4f71-3ed7-4e46c0c686c8>
2012-04-07 17:55:12 +0000

    again

---
+++ HWMaven1/src/main/java/com/mycompany/hwmaven1/UncoveredClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hwmaven1;
+
+/**
+ *
+ * @author ean
+ */
+public class UncoveredClass {
+    public void ReturnWhatever()
+    {
+    //whatever here    
+    }
+    
+}
