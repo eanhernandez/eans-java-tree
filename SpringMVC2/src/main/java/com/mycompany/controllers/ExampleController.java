916b79a09f5f75ad441814ca394a7b4755e90f8f
eanhernandez@eanh.net <eanhernandez@eanh.net@225b7d01-1364-4f71-3ed7-4e46c0c686c8>
2012-04-25 06:39:38 +0000

    made dummy tests

---
+++ SpringMVC2/src/main/java/com/mycompany/controllers/ExampleController.java
@@ -0,0 +1,18 @@
+package com.mycompany.controllers;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+ 
+public class ExampleController extends AbstractController {
+
+    protected ModelAndView handleRequestInternal(
+        HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        ModelAndView modelAndView = new ModelAndView("example", "message", "Spring MVC Example");
+ 
+        return modelAndView;
+    }
+ 
+}
